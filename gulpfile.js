a59c170f8742a6f922bb4a9fba521139f09d225b
Johnny <jdcabarbosa@gmail.com>
2018-07-31 11:13:38 +0100

    teste p/ ficheiros temp

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,32 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
 
-gulp.task('default', function () {
-    console.log('Boa - criaste uma task do Gulp.');    
+gulp.task('default', function() {
+  console.log('Hooray - you created a Gulp task.');
 });
 
-gulp.task('html', function () {
-    console.log('Imagina algo útil a fazer com o teu html aqui.');
+gulp.task('html', function() {
+  console.log('Imagine something useful being done to your HTML here.');
 });
 
-gulp.task('styles', function () {
-    //aqui definimos o ficheiro de origem e o de destino para ondem queremos direcionar
-    // o return é para garantir que o gulp corre dado ser assincrono
-    return gulp.src('.app/assets/styles/styles.css').pipe(gulp.dest('./app/temp/styles'));
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
 
-    watch('./app/index.html', function () {
-        gulp.start('html');        
-    });
+  watch('./app/index.html', function() {
+    gulp.start('html');
+  });
 
-    watch('./app/assets/styles/**/*.css', function () {
-        gulp.start('styles');       
-    })
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('styles');
+  });
 
 });
